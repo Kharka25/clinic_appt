165dfd9558fed875eb6f71a57fbd5c671247e796
Kharka <adewoyindaniel6@gmail.com>
2024-05-17 00:43:56 +0100

    Initial commit

---
+++ src/main/java/example/clinicappt/ClinicApptApplication.java
@@ -0,0 +1,13 @@
+package example.clinicappt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicApptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicApptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/clinicappt/ClinicApptApplicationTests.java
@@ -0,0 +1,13 @@
+package example.clinicappt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
