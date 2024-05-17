fdc6712e046bb657391fcdf891a72ef17094d093
Kharka <adewoyindaniel6@gmail.com>
2024-05-17 01:06:31 +0100

    Set up test environment

---
+++ src/test/java/example/clinicappt/ClinicApptJsonTest.java
@@ -0,0 +1,12 @@
+package example.clinicappt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClinicApptJsonTest {
+    @Test
+    void firstTest() {
+        assertThat(41 + 1).isEqualTo(42);
+    }
+}
